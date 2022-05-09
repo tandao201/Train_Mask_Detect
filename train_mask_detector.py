d5759288806cb9aa452d7c53dacb9ab350971fb1
tandao201 <tandao463@gmail.com>
2022-05-09 17:50:37 +0700

    change train_mask_detector

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -1,5 +1,5 @@
-# import the necessary packages
-import tensorflow as tf
+
+from venv import create
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.mobilenet_v2 import MobileNetV2
 from keras.layers import AveragePooling2D
@@ -22,123 +22,130 @@ import numpy as np
 import argparse
 import os
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-d", "--dataset", required=True,
-	help="path to input dataset")
-ap.add_argument("-p", "--plot", type=str, default="plot.png",
-	help="path to output loss/accuracy plot")
-ap.add_argument("-m", "--model", type=str,
-	default="mask_detector.model",
-	help="path to output face mask detector model")
-args = vars(ap.parse_args())
 
-# initialize the initial learning rate, number of epochs to train for,
-# and batch size
-INIT_LR = 1e-4
-EPOCHS = 20
-BS = 32
+INIT_LR = 1e-4 # toc do huan luyen mang noron.
+EPOCHS = 20 # được tính là khi chúng ta đưa tất cả dữ liệu trong tập train vào mạng neural network 1 lần
+BS = 32 # số lượng mẫu dữ liệu trong một lần huấn luyện
 
-# grab the list of images in our dataset directory, then initialize
-# the list of data (i.e., images) and class images
-print("[INFO] loading images...")
-imagePaths = list(paths.list_images(args["dataset"]))
-data = []
-labels = []
-# loop over the image paths
-for imagePath in imagePaths:
-	# extract the class label from the filename
-	label = imagePath.split(os.path.sep)[-2]
-	# load the input image (224x224) and preprocess it
-	image = load_img(imagePath, target_size=(224, 224))
-	image = img_to_array(image)
-	image = preprocess_input(image)
-	# update the data and labels lists, respectively
-	data.append(image)
-	labels.append(label)
-# convert the data and labels to NumPy arrays
-data = np.array(data, dtype="float32")
-labels = np.array(labels)
+def init_argument():
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-d", "--dataset", required=True,
+		help="path to input dataset")
+	ap.add_argument("-p", "--plot", type=str, default="plot.png",
+		help="path to output loss/accuracy plot")
+	ap.add_argument("-m", "--model", type=str,
+		default="mask_detector.model",
+		help="path to output face mask detector model")
+	args = vars(ap.parse_args())
+	return args
 
-# perform one-hot encoding on the labels
-lb = LabelBinarizer()
-labels = lb.fit_transform(labels)
-labels = to_categorical(labels)
-# partition the data into training and testing splits using 80% of
-# the data for training and the remaining 20% for testing
-(trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
-# construct the training image generator for data augmentation
-aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
+
+def convert_image():
+	data = []
+	labels = []
+	print("[INFO] loading images...")
+	imagePaths = list(paths.list_images(args["dataset"]))
+	for imagePath in imagePaths:
+		# extract the class label from the filename
+		label = imagePath.split(os.path.sep)[-2] # with mask or without mask
+		image = load_img(imagePath, target_size=(224, 224))
+		image = img_to_array(image)
+		image = preprocess_input(image)
+		data.append(image)
+		labels.append(label)
+	data = np.array(data, dtype="float32")
+	labels = np.array(labels)
+	return data, labels	
+
+def data_gen():
+	aug = ImageDataGenerator(
+	rotation_range=20, 
+	zoom_range=0.15, 
+	width_shift_range=0.2, 
 	height_shift_range=0.2,
 	shear_range=0.15,
 	horizontal_flip=True,
-	fill_mode="nearest")
+	fill_mode="nearest") # phương pháp bù lại ccho những pixel bị mất xung quanh
+	return aug
 
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
-baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
-# construct the head of the model that will be placed on top of the
-# the base model
-headModel = baseModel.output
-headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
-headModel = Flatten(name="flatten")(headModel)
-headModel = Dense(128, activation="relu")(headModel)
-headModel = Dropout(0.5)(headModel)
-headModel = Dense(2, activation="softmax")(headModel)
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
-model = Model(inputs=baseModel.input, outputs=headModel)
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
-for layer in baseModel.layers:
-	layer.trainable = False
+def create_model():
+	baseModel = MobileNetV2(
+		weights="imagenet", # Sử dụng các weights được train trên tập imagenet
+		include_top=False, # Loại bỏ các FC layers ở cuối
+		input_tensor=Input(shape=(224, 224, 3))
+	)
+	# Đóng băng các layers của pre-trained model, không cho chúng update
+	for layer in baseModel.layers:
+		layer.trainable = False
+	
+	#Tạo head model: là các FC layers được thêm vào làm nhiệm vụ phân loại dựa theo tập dữ liệu mới.
+	headModel = baseModel.output
+	headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+	headModel = Flatten(name="flatten")(headModel)
+	headModel = Dense(128, activation="relu")(headModel)
+	headModel = Dropout(0.5)(headModel)
+	headModel = Dense(2, activation="softmax")(headModel)
+	
+	# đặt head model lên đầu base model tạo được model chúng ta sử dụng train
+	model = Model(inputs=baseModel.input, outputs=headModel)
+	# tối ưu hóa
+	opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+	#  Định cấu hình quá trình học tập của mô hình
+	model.compile(loss="binary_crossentropy", optimizer=opt,
+		metrics=["accuracy"]) 
+	return model	
 
-# compile our model
-print("[INFO] compiling model...")
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+def create_plot_chart(N,H):
+	plt.style.use("ggplot")
+	plt.figure()
+	plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+	plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+	plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+	plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+	plt.title("Training Loss and Accuracy")
+	plt.xlabel("Epoch #")
+	plt.ylabel("Loss/Accuracy")
+	plt.legend(loc="lower left")
+	plt.savefig(args["plot"])
+# khoi tao tham so va bien dich lai tham so.
+args = init_argument() 
+# chuyen image thanh so kieu numpy, tach label
+data, labels = convert_image() 
 
-# train the head of the network
-print("[INFO] training head...")
-H = model.fit(
+# mã hóa các nhãn
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels) # chuyển vector sang ma trận nhị phân
+
+# chuyển dataset thành traning data (80%) và test data  (20%)
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+# biến đổi ảnh gốc thành nhiều ảnh khác nhau
+aug = data_gen()
+
+model = create_model()
+H = model.fit( # dao tao mo hinh (H: history )
 	aug.flow(trainX, trainY, batch_size=BS),
 	steps_per_epoch=len(trainX) // BS,
 	validation_data=(testX, testY),
 	validation_steps=len(testX) // BS,
 	epochs=EPOCHS)
 
-# make predictions on the testing set
+#test
 print("[INFO] evaluating network...")
 predIdxs = model.predict(testX, batch_size=BS)
 
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
-predIdxs = np.argmax(predIdxs, axis=1)
+# tìm chỉ số của nhãn với xác suất dự đoán lớn nhất tương ứng trong mỗi bức ảnh test.
+predIdxs = np.argmax(predIdxs, axis=1) 
 
-# show a nicely formatted classification report
+# tạo report cho test model
 print(classification_report(testY.argmax(axis=1), predIdxs,
 	target_names=lb.classes_))
 
-# serialize the model to disk
+# Lưu model
 print("[INFO] saving mask detector model...")
 model.save(args["model"], save_format="h5")
 
-# plot the training loss and accuracy
 N = EPOCHS
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
+create_plot_chart(N,H)
